1b18f5bd8733d8e4c447a4ee1ff9a20ab4116c9c
Pluggito <pluggitoshfl5050@gmail.com>
2025-12-17 11:57:43 +0100

    feat: new fix

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -58,7 +58,7 @@ export async function getDbUserId() {
 
   const user = await getUserByClerkId(clerkId);
 
-  if (!user) throw new Error("User not found");
+  if (!user) return null; // User not synced to DB yet, return null instead of throwing
 
   return user.id;
 }
